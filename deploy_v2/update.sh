9fb4f79f3c3d6781e507981d45ea4621f1b2ec0d
Madhu <madhummr2000@gmail.com>
2023-01-30 11:31:06 +0530

    changes in update.sh

--- deploy_v2/update.sh
+++ deploy_v2/update.sh
@@ -7,6 +7,8 @@ PROJECT_BASE_PATH='/user/local/apps/profiles-rest-api'
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py makemigrations
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
 supervisorctl restart profiles_api
 
 echo "DONE! :)"
+cd
