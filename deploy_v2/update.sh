b8bd28e1fa9f94c16e7a028760337c9654c715c4
Madhu <madhummr2000@gmail.com>
2023-01-31 22:49:04 +0530

    Mistakes in the setup file is corrected

--- deploy_v2/update.sh
+++ deploy_v2/update.sh
@@ -1,25 +1,8 @@
 #!/usr/bin/env bash
-#!/bin/bash
-
-# Your existing code
-...
-
-# Activate the virtual environment
-source $VIRTUAL/bin/activate
-
-# Access the Python binary inside the virtual environment
-python manage.py makemigrations
-
-# Deactivate the virtual environment
-deactivate
-
-# Your existing code
-...
-
 
 set -e
 
-PROJECT_BASE_PATH='/user/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
